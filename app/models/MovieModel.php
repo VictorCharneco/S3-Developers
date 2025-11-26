7ff996166b0858c56db830e2adc870f4370eaa24
Victor C.B. <victor.charneco.barba@gmail.com>
2025-11-26 16:24:22 +0100

    Movie Model + movies.json created

---
+++ app/models/MovieModel.php
@@ -0,0 +1,25 @@
+<?php
+
+    class MovieModel{
+
+        //json?
+
+        public function __construct(){
+
+        }
+
+        public function addMovie(){
+
+        }
+
+        public function deleteMovie(){
+
+        }
+
+        public function showMovies(){
+
+        }
+
+    }
+
+?>
